bhalevy/scylla

tablets: read_tablet_mutations: use unfreeze_and_split_gently
Split the tablets mutations by number of rows, based on
`min_tablets_in_mutation` (currently calibrated to 1024),
similar to the splitting done in
`storage_service::merge_topology_snapshot`.

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

replica/tablets.cc
@@ -355,15 +355,13 @@ future<std::unordered_set<locator::host_id>> read_required_hosts(cql3::query_pro
     co_return std::move(hosts);
 }
 
-future<std::vector<canonical_mutation>> read_tablet_mutations(seastar::sharded<replica::database>& db) {
+future<> read_tablet_mutations(seastar::sharded<replica::database>& db, std::function<void(mutation)> process_mutation) {
     auto s = db::system_keyspace::tablets();
     auto rs = co_await db::system_keyspace::query_mutations(db, db::system_keyspace::NAME, db::system_keyspace::TABLETS);
-    std::vector<canonical_mutation> result;
-    result.reserve(rs->partitions().size());
+    constexpr size_t max_rows = min_tablets_in_mutation;
     for (auto& p: rs->partitions()) {
-        result.emplace_back(canonical_mutation(co_await p.mut().unfreeze_gently(s)));
+        co_await p.mut().unfreeze_and_split_gently(s, max_rows, process_mutation);
     }
-    co_return std::move(result);
 }
 
 // This sstable set provides access to all the stables in the table, using a snapshot of all

replica/tablets.hh
@@ -69,7 +69,7 @@ future<locator::tablet_metadata> read_tablet_metadata(cql3::query_processor&);
 future<std::unordered_set<locator::host_id>> read_required_hosts(cql3::query_processor&);
 
 /// Reads tablet metadata from system.tablets in the form of mutations.
-future<std::vector<canonical_mutation>> read_tablet_mutations(seastar::sharded<database>&);
+future<> read_tablet_mutations(seastar::sharded<database>&, std::function<void(mutation)> process_mutation);
 
 /// Reads tablet transition stage (if any)
 future<std::optional<locator::tablet_transition_stage>> read_tablet_transition_stage(cql3::query_processor& qp, table_id tid, dht::token last_token);

service/migration_manager.cc
@@ -164,9 +164,9 @@ void migration_manager::init_messaging_service()
             if (options->group0_snapshot_transfer) {
                 cm.emplace_back(co_await db::system_keyspace::get_group0_history(db));
                 if (proxy.local().local_db().get_config().check_experimental(db::experimental_features_t::feature::TABLETS)) {
-                    for (auto&& m: co_await replica::read_tablet_mutations(db)) {
+                    co_await replica::read_tablet_mutations(db, [&] (mutation m) {
                         cm.emplace_back(std::move(m));
-                    }
+                    });
                 }
             }
 

test/perf/perf_tablets.cc
@@ -124,10 +124,12 @@ static future<> test_basic_operations(app_template& app) {
 
         std::vector<canonical_mutation> muts;
         auto time_to_read_muts = duration_in_seconds([&] {
-            muts = replica::read_tablet_mutations(e.local_qp().proxy().get_db()).get();
+            replica::read_tablet_mutations(e.local_qp().proxy().get_db(), [&] (mutation m) {
+                muts.emplace_back(m);
+            }).get();
         });
 
-        testlog.info("Read mutations in {:.6f} [ms]", time_to_read_muts.count() * 1000);
+        testlog.info("Read mutations in {:.6f} [ms] {} mutations", time_to_read_muts.count() * 1000, muts.size());
 
         auto time_to_read_hosts = duration_in_seconds([&] {
             replica::read_required_hosts(e.local_qp()).get();

mananbaig/scyllaDB

replica/tablets.cc
@@ -357,15 +357,15 @@ future<std::unordered_set<locator::host_id>> read_required_hosts(cql3::query_pro
     co_return std::move(hosts);
 }
 
-future<std::vector<canonical_mutation>> read_tablet_mutations(seastar::sharded<replica::database>& db) {
+future<> read_tablet_mutations(seastar::sharded<replica::database>& db, std::function<void(canonical_mutation)> process_mutation) {
     auto s = db::system_keyspace::tablets();
     auto rs = co_await db::system_keyspace::query_mutations(db, db::system_keyspace::NAME, db::system_keyspace::TABLETS);
-    std::vector<canonical_mutation> result;
-    result.reserve(rs->partitions().size());
+    constexpr size_t max_rows = min_tablets_in_mutation;
     for (auto& p: rs->partitions()) {
-        result.emplace_back(co_await make_canonical_mutation_gently(co_await unfreeze_gently(p.mut(), s)));
+        co_await unfreeze_and_split_gently(p.mut(), s, max_rows, [&] (mutation m) -> future<> {
+            process_mutation(co_await make_canonical_mutation_gently(m));
+        });
     }
-    co_return std::move(result);
 }
 
 // This sstable set provides access to all the stables in the table, using a snapshot of all

replica/tablets.hh
@@ -69,7 +69,7 @@ future<locator::tablet_metadata> read_tablet_metadata(cql3::query_processor&);
 future<std::unordered_set<locator::host_id>> read_required_hosts(cql3::query_processor&);
 
 /// Reads tablet metadata from system.tablets in the form of mutations.
-future<std::vector<canonical_mutation>> read_tablet_mutations(seastar::sharded<database>&);
+future<> read_tablet_mutations(seastar::sharded<database>&, std::function<void(canonical_mutation)> process_mutation);
 
 /// Reads tablet transition stage (if any)
 future<std::optional<locator::tablet_transition_stage>> read_tablet_transition_stage(cql3::query_processor& qp, table_id tid, dht::token last_token);

service/migration_manager.cc
@@ -160,9 +160,9 @@ void migration_manager::init_messaging_service()
             if (options->group0_snapshot_transfer) {
                 cm.emplace_back(co_await db::system_keyspace::get_group0_history(db));
                 if (proxy.local().local_db().get_config().check_experimental(db::experimental_features_t::feature::TABLETS)) {
-                    for (auto&& m: co_await replica::read_tablet_mutations(db)) {
+                    co_await replica::read_tablet_mutations(db, [&] (canonical_mutation m) {
                         cm.emplace_back(std::move(m));
-                    }
+                    });
                 }
             }
 

test/perf/perf_tablets.cc
@@ -125,10 +125,12 @@ static future<> test_basic_operations(app_template& app) {
 
         std::vector<canonical_mutation> muts;
         auto time_to_read_muts = duration_in_seconds([&] {
-            muts = replica::read_tablet_mutations(e.local_qp().proxy().get_db()).get();
+            replica::read_tablet_mutations(e.local_qp().proxy().get_db(), [&] (canonical_mutation m) {
+                muts.emplace_back(m);
+            }).get();
         });
 
-        testlog.info("Read mutations in {:.6f} [ms]", time_to_read_muts.count() * 1000);
+        testlog.info("Read mutations in {:.6f} [ms] {} mutations", time_to_read_muts.count() * 1000, muts.size());
 
         auto time_to_read_hosts = duration_in_seconds([&] {
             replica::read_required_hosts(e.local_qp()).get();